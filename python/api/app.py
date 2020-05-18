d979b50745ca14613ed90b3665e1161d1b8d27e2
velaraptor <info@christophvel.com>
2020-05-18 00:52:19 -0500

    update api with response data

--- python/api/app.py
+++ python/api/app.py
@@ -1,15 +1,69 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restx import Api, Resource, fields
 import os
 
+VERSION = os.getenv('VERSION')
+api_v1 = Blueprint('api', __name__, url_prefix=f'/api/v{VERSION}/')
+api = Api(api_v1, version='v%s' % VERSION, title='POCAS API',
+          description='an api to get results',
+          )
 
-app = Flask(__name__)
-api = Api(app, version=os.getenv('VERSION'), title='POCAS API',
-    description='an api to get data',
+ns = api.namespace('top_results', description='recieve questionaire and get results')
+
+
+top_results = api.model(
+    'TopResults', {'id': fields.Integer(required=True, description="Unique id of Service", example=1),
+                   'name': fields.String(required=True, description="The Name of Service", example='Indigent Legal Fund'),
+                   'phone': fields.Integer(required=True, description="Phone Number of service"),
+                   'address': fields.String(required=False, description="the address of service", example='1111 S 1st'),
+                   'general_topic': fields.String(required=True, description="the general topic service belongs to",
+                                                  example='Immigration'),
+                   'tags': fields.List(fields.String(required=False, description='tag', example='Young Adult'),
+                                       required=True, description='tags associated'),
+                   'city': fields.String(required=False, description="city service is in", example='Tuscon'),
+                   'state': fields.String(required=False, description="state service is in", example='AZ'),
+                   'lat': fields.Float(required=False, description="Latitude coordinates", example=72.34),
+                   'lon': fields.Float(required=False, description="Longitude coordinates", example=34.01),
+                   'zip_code': fields.Integer(required=False, description='zip code for service', example=78724),
+                   'web_site': fields.String(required=False, description='web site for service',
+                                          example='http://www.example.com'),
+                   'pocas_score': fields.Float(required=True, description='score for ordering results, higher is better',
+                                               example=0.12)
+                   }
 )
 
-ns = api.namespace('person', description='TODO operations')
+
+parser = api.parser()
+parser.add_argument(
+    "address", type=str, required=True, help="The task details", location="form"
+)
+parser.add_argument(
+    "dob", type=str, required=True, help="The task details", location="form"
+)
+parser.add_argument(
+    "question_answers", type=str, required=True, help="The task details", location="form"
+)
+
+TEST = {'id': 1, 'name': 'test', 'phone': 5551114444, 'general_topic': 'test', 'tags': ['tag1', 'tag2'],
+        'web_site': 'http://www.example.com', 'pocas_score': 0.98}
+
+
+@ns.route("/")
+class TopResults(Resource):
+    @api.doc(parser=parser)
+    @api.marshal_with(top_results)
+    def post(self):
+        args = parser.parse_args()
+        return TEST
+
+    @api.marshal_with(top_results)
+    def get(self):
+        return TEST
 
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config['RESTX_MASK_SWAGGER'] = False
+
+    app.register_blueprint(api_v1)
     app.run(debug=True, port=80, host='0.0.0.0')
