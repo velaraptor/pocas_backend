154d9e18fdcc5343d798bcc895172f6960cc42ae
velaraptor <info@christophvel.com>
2020-06-12 17:36:28 -0500

    HOT FIX tags query

--- app/cosine_search/top_results.py
+++ app/cosine_search/top_results.py
@@ -208,7 +208,7 @@ class GetTopNResults:
             self.tags = ['Public Benefits']
         m = MongoConnector()
         top_results = m.query_results(db=DB_SERVICES['db'], collection=DB_SERVICES['collection'],
-                                      query={'tags': {'$in': self.tags}},
+#                                      query={'tags': {'$in': self.tags}},
                                       exclude={'loc': 0})
         self.log().debug(self.__dict__)
         try:
