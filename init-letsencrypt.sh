54e485868a283d71c98ca1d8464da7b6d99ba419
velaraptor <info@christophvel.com>
2022-09-15 13:06:18 -0500

    init fix shell

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(mhpportal.app.org)
+domains=(mhpportal.app)
 rsa_key_size=4096
 data_path="./c/certbot"
 email="info@christophvel.com" # Adding a valid address is strongly recommended
